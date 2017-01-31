b6e7cf00eee3fba93363abe9f13ec6106e626a19
Steen Lehmann <steen.lehmann@gmail.com>
2017-01-31 20:39:38 +0100

    Standalone web page (remove server, crud)

--- src/intercom.js
+++ src/intercom.js
@@ -1,7 +1,5 @@
 import Rx from 'rx'
 
-export const textnodes = new Rx.Subject()
-
 // Visual items updated are posted here - SVG DOM elements
 export const visual = new Rx.Subject()
 
@@ -17,8 +15,6 @@ export const tickers = []
 export const state$ = new Rx.ReplaySubject(1)
 export const action$ = new Rx.Subject()
 
-action$.subscribe(e => { console.log('actions', e) })
-textnodes.subscribe(e => { console.log('textnodes', e) })
 visual.subscribe(e => { console.log('visual', e) })
 drags.subscribe(e => { console.log('drags', e) })
 zooms.debounce(100).subscribe(e => { console.log('zooms', e) })
--- src/visibility.js
+++
@@ -1,9 +0,0 @@
-import Rx from 'rx'
-import { zooms, state$, visibleNodes } from './intercom'
-
-/* Combine zooms and model changes to produce an event stream of visibleNodes */
-Rx.Observable.combineLatest(
-  zooms.debounce(300), state$.filter(s => s.has('units')),
-    (zoom, current) => current.get('units')
-  ).subscribe(visibleNodes)
-
